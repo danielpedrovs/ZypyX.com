4b7f2e4e644bec9400288ea2d63e5d9d8cbdcf8f
Daniel Pedro Sousa <danielpedrovs@gmail.com>
2024-10-16 01:19:37 +0000

    this is the first version of ZYPYX site

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("enter-button").addEventListener("click", function() {
+    document.getElementById("overlay").style.display = "none";
+    document.getElementById("main-content").style.display = "block";
+});
